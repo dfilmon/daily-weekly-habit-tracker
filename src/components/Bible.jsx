ddb121379f6721a465ded458f1d7b4038227d398
dfilmon <abraha.filmon@gmail.com>
2022-12-03 09:56:43 -0500

    1 file modified

--- src/components/Bible.jsx
+++ src/components/Bible.jsx
@@ -2,105 +2,86 @@ import React from 'react';
 
 export default function Bible() {
   return (
-    <div className="bg-gray-50">
-      <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
+    <div className="bg-gray-50 p-12">
+      <div className="mx-auto max-w-7xl py-4 px-4 sm:px-16 lg:flex lg:items-center lg:justify-between lg:pt-16 lg:px-20">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           <span className="block">Bible Readings</span>
-          <span className="block text-indigo-600">
+          <span className="block text-indigo-600 text-2xl">
             Daily progress documented here.
           </span>
         </h2>
-        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-          <div className="inline-flex rounded-md shadow">
-            <a
-              href="#"
-              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700"
-            >
-              Get started
-            </a>
-          </div>
-          <div className="ml-3 inline-flex rounded-md shadow">
-            <a
-              href="#"
-              className="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 hover:bg-indigo-50"
-            >
-              Learn more
-            </a>
-          </div>
-        </div>
 
+
+    
       </div>
-      <div className="bg-gray-50">
-          <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl">
-              Saturday
-            </h2>
-            <p className="text-md font-bold tracking-tight text-gray-900 sm:text-md">
-              1 Corinthians
-            </p>
-            
 
-          </div>
+      <div className="container p-8 mx-auto max-w-7xl py-12 px-4 sm:px-6  lg:items-center lg:justify-between lg:py-6 lg:px-8">
+        <div className="date">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-2xl mx-12">
+            Saturday
+          </h2>
+          <p className="text-md font-bold tracking-tight text-gray-900 sm:text-md mx-12">
+            1 Corinthians
+          </p>
         </div>
-      <div className="container flex">
-            <p>
-              1-2 ኣሕዋተየ፡ ኣነ ድማ ኣብ ማእከልኩም ከሎኹ፡ ብዘይ የሱስ ክርስቶስ፡ ንሱ ኸኣ እቲ እተሰቕለ፡ ሓደ
-              ነገር እኳ ኸም ዘይፈልጥ መደብኩ እሞ፡ ናባኻትኩም ከመጽእ ከሎኹ፡ ምስክር ኣምላኽ ብብሉጽ ዘረባ ወይስ
-              ብጥበብ ክነግረኩም ኣይመጻእኩን። 3 ኣነ ኸኣ ብድኻምን ብፍርሃትን ብብዙሕ ምምብድባድን እየ ኣባኻትኩም
-              ዝነበርኩ። 4-5 እምነትኩም ብስልጣን ኣምላኽ እምበር፡ ብጥበብ ሰብ ምእንቲ ኸይትስረትሲ፡ ዘረባይን
-              ስብከተይን ድማ ብምርዳእ መንፈስን ስልጣንን እምበር፡ ብዜስድዕ ዘረባ ጥበብ ኣይነበረን። 6-7 እምብኣርስ
-              ኣብ ማእከል እቶም እኹላት ጥበብ ንዛረብ ኢና፡ እቲ ኣምላኽ ቅድሚ ዘመናት ንኽብረትና ዘዳለዎ፡ እቲ ናይ
-              ኣምላኽ ሕቡእ ምስጢር ጥበብ ኢና እንዛረብ ዘሎና እምበር፡ ጥበብ እዛ ዓለም እዚኣ ወይስ ናይቶም ዚሐልፉ
-              ገዛእቲ እዛ ዓለም እዚኣ ኣይንዛረብን ኢና። 8 ካብ ገዛእቲ እዛ ዓለም እዚኣ ሓደ እኳ ነዚ ጥበብዚ
-              ኣይፈለጦን፡ እንተ ዚፈልጥዎስ፡ ንጐይታ ኽብሪ ኣይምሰቐልዎን ነይሮም። 9 ግናኸ ከምቲ ጽሑፍ፡ እቲ ዓይኒ
-              ዘይረኣየቶ እዝኒ ኸኣ ዘይሰምዓቶ ኣብ ልቢ ሰብ ድማ ዘይተሐስበ፡ ኣምላኽ ነቶም ዜፍቅርዎ ኣዳለወሎም፡
-              ዚብል፡ 10 እቲ መንፈስ ግና መዓሙቚ ኣምላኽ እኳ ኸይተረፈ ንዂሉ ይምርምር እዩ እሞ፡ ንኣና ኣምላኽ
-              ብመንፈሱ ገይሩ ገለጸልና። 11 እቲ ኣብኡ ዘሎ መንፈስ እንተ ዘይኰይኑስ፡ ኣብ ሰብ ዘሎ ነገር ዚፈልጥከ
-              እንታዋይ ሰብ እዩ፧ ከምኡውን መንፈስ ኣምላኽ እንተ ዘይኰይኑስ፡ ነቲ ኣብ ኣምላኽ ዘሎ ሓደ እኳ ዚፈልጦ
-              የልቦን። 12 ንሕና ግና፡ እቲ ብኣምላኽ እተዋህበና ምእንቲ ኽንፈልጥሲ፡ ነቲ ኻብ ኣምላኽ ዚኸውን መንፈስ
-              እምበር፡ መንፈስ ዓለም ኣይተቐበልናን። 13 ንሕና ኸኣ መንፈሳዊ ነገር ምስ መንፈሳዊ እናኣሰማማዕና፡
-              መንፈስ ቅዱስ ብዚምህረሉ ቓል እምበር፡ ጥበብ ሰብ ብዚምህረሉ ቓል ኣይኰንናን እንዛረብ ዘሎና። 14 እቲ
-              ስጋዊ ሰብ ነቲ ኣብ መንፈስ ኣምላኽ ዘሎ ኣይቅበሎን፡ ብመንፈስ ዚምርመር ስለ ዝዀነ፡ ንእኡ ዕሽነት እዩ
-              እሞ፡ ኬስተውዕሎ ኣይከኣሎን እዩ። 15-16 ንእግዚኣብሄር ኪምህሮ ኢሉ፡ ሓሳቡ ዝፈለጠኸ እንታዋይ እዩ፧
-              ንሕና ግና ሓሳብ ክርስቶስ ኣሎና። እምብኣርሲ እቲ መንፈሳዊ ንዂሉ ይምርምር፡ ንሱ ግና ብሓደ እኳ
-              ኣይምርመርን እዩ።
-            </p>
-  
-            <p>
-              1 When I came to you, brothers, I didn''t come with excellence of
-              speech or of wisdom, proclaiming to you the testimony of God. 2
-              For I determined not to know anything among you, except Jesus
-              Christ, and him crucified. 3 I was with you in weakness, in fear,
-              and in much trembling. 4 My speech and my preaching were not in
-              persuasive words of human wisdom, but in demonstration of the
-              Spirit and of power, 5 that your faith wouldn''t stand in the
-              wisdom of men, but in the power of God. 6 We speak wisdom,
-              however, among those who are full grown; yet a wisdom not of this
-              world, nor of the rulers of this world, who are coming to nothing.
-              7 But we speak God''s wisdom in a mystery, the wisdom that has
-              been hidden, which God foreordained before the worlds for our
-              glory, 8 which none of the rulers of this world has known. For had
-              they known it, they wouldn''t have crucified the Lord of glory. 9
-              But as it is written, "Things which an eye didn''t see, and an ear
-              didn''t hear, which didn''t enter into the heart of man, these God
-              has prepared for those who love him." 10 But to us, God revealed
-              them through the Spirit. For the Spirit searches all things, yes,
-              the deep things of God. 11 For who among men knows the things of a
-              man, except the spirit of the man, which is in him? Even so, no
-              one knows the things of God, except God''s Spirit. 12 But we
-              received, not the spirit of the world, but the Spirit which is
-              from God, that we might know the things that were freely given to
-              us by God. 13 Which things also we speak, not in words which
-              man''s wisdom teaches, but which the Holy Spirit teaches,
-              comparing spiritual things with spiritual things. 14 Now the
-              natural man doesn''t receive the things of God''s Spirit, for they
-              are foolishness to him, and he can''t know them, because they are
-              spiritually discerned. 15 But he who is spiritual discerns all
-              things, and he himself is judged by no one. 16 "For who has known
-              the mind of the Lord, that he should instruct him?" But we have
-              Christ''s mind.
-            </p>
-            </div>
+        <div className="readings flex mt-8">
+          <p className="mx-12">
+            1-2 ኣሕዋተየ፡ ኣነ ድማ ኣብ ማእከልኩም ከሎኹ፡ ብዘይ የሱስ ክርስቶስ፡ ንሱ ኸኣ እቲ እተሰቕለ፡ ሓደ
+            ነገር እኳ ኸም ዘይፈልጥ መደብኩ እሞ፡ ናባኻትኩም ከመጽእ ከሎኹ፡ ምስክር ኣምላኽ ብብሉጽ ዘረባ ወይስ
+            ብጥበብ ክነግረኩም ኣይመጻእኩን። 3 ኣነ ኸኣ ብድኻምን ብፍርሃትን ብብዙሕ ምምብድባድን እየ ኣባኻትኩም
+            ዝነበርኩ። 4-5 እምነትኩም ብስልጣን ኣምላኽ እምበር፡ ብጥበብ ሰብ ምእንቲ ኸይትስረትሲ፡ ዘረባይን
+            ስብከተይን ድማ ብምርዳእ መንፈስን ስልጣንን እምበር፡ ብዜስድዕ ዘረባ ጥበብ ኣይነበረን። 6-7 እምብኣርስ
+            ኣብ ማእከል እቶም እኹላት ጥበብ ንዛረብ ኢና፡ እቲ ኣምላኽ ቅድሚ ዘመናት ንኽብረትና ዘዳለዎ፡ እቲ ናይ
+            ኣምላኽ ሕቡእ ምስጢር ጥበብ ኢና እንዛረብ ዘሎና እምበር፡ ጥበብ እዛ ዓለም እዚኣ ወይስ ናይቶም ዚሐልፉ
+            ገዛእቲ እዛ ዓለም እዚኣ ኣይንዛረብን ኢና። 8 ካብ ገዛእቲ እዛ ዓለም እዚኣ ሓደ እኳ ነዚ ጥበብዚ
+            ኣይፈለጦን፡ እንተ ዚፈልጥዎስ፡ ንጐይታ ኽብሪ ኣይምሰቐልዎን ነይሮም። 9 ግናኸ ከምቲ ጽሑፍ፡ እቲ ዓይኒ
+            ዘይረኣየቶ እዝኒ ኸኣ ዘይሰምዓቶ ኣብ ልቢ ሰብ ድማ ዘይተሐስበ፡ ኣምላኽ ነቶም ዜፍቅርዎ ኣዳለወሎም፡ ዚብል፡
+            10 እቲ መንፈስ ግና መዓሙቚ ኣምላኽ እኳ ኸይተረፈ ንዂሉ ይምርምር እዩ እሞ፡ ንኣና ኣምላኽ ብመንፈሱ ገይሩ
+            ገለጸልና። 11 እቲ ኣብኡ ዘሎ መንፈስ እንተ ዘይኰይኑስ፡ ኣብ ሰብ ዘሎ ነገር ዚፈልጥከ እንታዋይ ሰብ እዩ፧
+            ከምኡውን መንፈስ ኣምላኽ እንተ ዘይኰይኑስ፡ ነቲ ኣብ ኣምላኽ ዘሎ ሓደ እኳ ዚፈልጦ የልቦን። 12 ንሕና
+            ግና፡ እቲ ብኣምላኽ እተዋህበና ምእንቲ ኽንፈልጥሲ፡ ነቲ ኻብ ኣምላኽ ዚኸውን መንፈስ እምበር፡ መንፈስ ዓለም
+            ኣይተቐበልናን። 13 ንሕና ኸኣ መንፈሳዊ ነገር ምስ መንፈሳዊ እናኣሰማማዕና፡ መንፈስ ቅዱስ ብዚምህረሉ ቓል
+            እምበር፡ ጥበብ ሰብ ብዚምህረሉ ቓል ኣይኰንናን እንዛረብ ዘሎና። 14 እቲ ስጋዊ ሰብ ነቲ ኣብ መንፈስ
+            ኣምላኽ ዘሎ ኣይቅበሎን፡ ብመንፈስ ዚምርመር ስለ ዝዀነ፡ ንእኡ ዕሽነት እዩ እሞ፡ ኬስተውዕሎ ኣይከኣሎን
+            እዩ። 15-16 ንእግዚኣብሄር ኪምህሮ ኢሉ፡ ሓሳቡ ዝፈለጠኸ እንታዋይ እዩ፧ ንሕና ግና ሓሳብ ክርስቶስ
+            ኣሎና። እምብኣርሲ እቲ መንፈሳዊ ንዂሉ ይምርምር፡ ንሱ ግና ብሓደ እኳ ኣይምርመርን እዩ።
+          </p>
+
+          <p className="mx-12">
+            1 When I came to you, brothers, I didn''t come with excellence of
+            speech or of wisdom, proclaiming to you the testimony of God. 2 For
+            I determined not to know anything among you, except Jesus Christ,
+            and him crucified. 3 I was with you in weakness, in fear, and in
+            much trembling. 4 My speech and my preaching were not in persuasive
+            words of human wisdom, but in demonstration of the Spirit and of
+            power, 5 that your faith wouldn''t stand in the wisdom of men, but
+            in the power of God. 6 We speak wisdom, however, among those who are
+            full grown; yet a wisdom not of this world, nor of the rulers of
+            this world, who are coming to nothing. 7 But we speak God''s wisdom
+            in a mystery, the wisdom that has been hidden, which God
+            foreordained before the worlds for our glory, 8 which none of the
+            rulers of this world has known. For had they known it, they
+            wouldn''t have crucified the Lord of glory. 9 But as it is written,
+            "Things which an eye didn''t see, and an ear didn''t hear, which
+            didn''t enter into the heart of man, these God has prepared for
+            those who love him." 10 But to us, God revealed them through the
+            Spirit. For the Spirit searches all things, yes, the deep things of
+            God. 11 For who among men knows the things of a man, except the
+            spirit of the man, which is in him? Even so, no one knows the things
+            of God, except God''s Spirit. 12 But we received, not the spirit of
+            the world, but the Spirit which is from God, that we might know the
+            things that were freely given to us by God. 13 Which things also we
+            speak, not in words which man''s wisdom teaches, but which the Holy
+            Spirit teaches, comparing spiritual things with spiritual things. 14
+            Now the natural man doesn''t receive the things of God''s Spirit,
+            for they are foolishness to him, and he can''t know them, because
+            they are spiritually discerned. 15 But he who is spiritual discerns
+            all things, and he himself is judged by no one. 16 "For who has
+            known the mind of the Lord, that he should instruct him?" But we
+            have Christ''s mind.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
