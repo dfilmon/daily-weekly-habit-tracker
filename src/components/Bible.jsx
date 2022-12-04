559a4011b5f72d8286c0deadfd9f11184bb3fe52
dfilmon <abraha.filmon@gmail.com>
2022-12-04 18:55:49 -0500

    1 file modified

--- src/components/Bible.jsx
+++ src/components/Bible.jsx
@@ -43,7 +43,6 @@ export default function Bible() {
             እዩ። 15-16 ንእግዚኣብሄር ኪምህሮ ኢሉ፡ ሓሳቡ ዝፈለጠኸ እንታዋይ እዩ፧ ንሕና ግና ሓሳብ ክርስቶስ
             ኣሎና። እምብኣርሲ እቲ መንፈሳዊ ንዂሉ ይምርምር፡ ንሱ ግና ብሓደ እኳ ኣይምርመርን እዩ።
           </p>
-
           <p className="mx-12">
             1 When I came to you, brothers, I didn''t come with excellence of
             speech or of wisdom, proclaiming to you the testimony of God. 2 For
@@ -76,6 +75,76 @@ export default function Bible() {
             all things, and he himself is judged by no one. 16 "For who has
             known the mind of the Lord, that he should instruct him?" But we
             have Christ''s mind.
+          </p>
+        </div>
+
+        <div className="readings flex mt-8">
+          <p className="mx-12">
+            1 ኣሕዋተየ፡ ኣነ ኸኣ ከም ንስጋውያን፡ ከም ንቘልዑ ብክርስቶስ እምበር፡ ከም ንመንፈሳውያን ክዛረበኩም
+            ኣይኰነለይን እዩ። 2-3 ገና ስጋውያን ስለ ዝዀንኩም፡ ጸባ መገብኩኹም፡ ዕጽም ብልዒ ኣይኰነን። ገና
+            ኣይከኣልኩምዎን፡ ሕጂ እኳ ኣይትኽእልዎን ኢኹም። ቅንኣት ባእስን ካብ ዚህልወኩምሲ፡ ስጋውያን ምዃንኩምዶ
+            ኣይኰነን፧ ከም ባህሊ ሰብዶ ትኸዱ ኣየለኹምን፧ 4 እቲ ሓደ፡ ኣነስ ናይ ጳውሎስ እየ፡ ሓደ ኻልእ ከኣ፡
+            ኣነስባ ናይ ኣጵሎስ እየ፡ ካብ ዚብል፡ ስጋውያን ምዃንኩምዶ ኣይኰነን፧ 5 እምብኣርከ፡ ኣጵሎስ እንታይ እዩ፧
+            ጳውሎስሲ እንታይ እዩ፧ ብኣታቶም ጌርኩም ናብ እምነት ዝበጻሕኩም ኣገልገልቲ እዮም፡ እዚ ድማ እግዚኣብሄር
+            ንነፍሲ ወከፎም ከም ዝሃቦ እዩ። 6 ኣነ ተኸልኩ ኣጵሎስ ኣስተየ፡ ኣምላኽ ግና ኣብቈለ። 7 እምብኣርሲ እቲ
+            ዜብቊል ኣምላኽ ደኣ እዩ እምበር፡ እቲ ዚተክል ወይስ እቲ ዜስቲ ዚዓብሱ የብሎምን። 8 እቲ ዚተክልን እቲ
+            ዜስትን ሓደ እዮም፡ ነፍሲ ወከፎም ከኣ ከከም ጻዕሩ ዓስቡ ኪቕበል እዩ። 9 ንሕና መዓይይቲ ኣምላኽ ኢና
+            እሞ፡ ንስኻትኩም ግራት ኣምላኽ፡ ህንጻ ኣምላኽ ኢኹም። 10-11 ብዘይ እቲ ስሩት መሰረት፡ ንሱ ኸኣ የሱስ
+            ክርስቶስ፡ ካልእ መሰረት ሓደ እኳ ኺስርት ዚኽእል የልቦን እሞ፡ ኣነ ኸምቲ እተዋህበኒ ጸጋ ኣምላኽ ብልሓተኛ
+            ሃናጺ ዀይነ ነቲ መሰረት ሰረትኩ፡ ሓደ ኻልእ ከኣ ኣብ ልዕሊኡ ይሃንጽ። ነፍሲ ወከፍውን ከመይ ገይሩ ኸም
+            ዚሃንጽ ይጠንቀቕ። 12 ሓደ እኳ ኣብ ልዕሊ እዛ መሰረት እዚኣ ወርቂ፡ ብሩር፡ ክቡር እምኒ፡ ዕጨይቲ፡ ሳዕሪ
+            ወይስ ሓሰር እንተ ሰርሔ፡ 13 እታ መዓልቲ እቲኣ ብሓዊ ኽትግለጽ እያ እሞ፡ ክትቀልዖ እያ። ግብሪ ነፍሲ
+            ወከፍ ኪግሃድ እዩ፡ ንግብሪ ነፍሲ ወከፍ ከኣ ከመይ ምዃኑ እቲ ሓዊ ኺፍትኖ እዩ። 14 ሰብ፡ እቲ ኣብ
+            ልዕሊኡ እተሃንጸ ዕዮ እንተ ጸንዓሉ፡ ዓስቢ ኪቕበል እዩ። 15 እቲ ዕዮኡ እንተ ነደደ ግና፡ ዓስቡ ኺስእን
+            እዩ። ንርእሱ ግና ኪድሕን፡ ግናኸ ከምቲ ሓዊ ሐሊፉ ዝደሐነ እዩ። 16 ቤተ መቕደስ ኣምላኽ ምዃንኩምን
+            መንፈስ ኣምላኽ ኣባኻትኩም ሐዲሩ ምህላዉንዶ ኣይትፈልጡን ኢኹም፧ 17 ሓደ እኳ ነታ ቤት መቕደስ ኣምላኽ
+            እንተ ኣማሰነ፡ ኣምላኽ ኬማስኖ እዩ። ቤተ መቕደስ ኣምላኽ ቅድስቲ እያ፡ እዛ ቤት መቕደስ እዚኣውን
+            ንስኻትኩም ኢኹም። 18-20 ሓደ እኳ ንርእሱ ኣይጠብራ። ነቶም ጥበበኛታት ብጒርሖም ይሕዞም እዩ፡ ዚብል
+            ጽሑፍ እዩ እሞ፡ ከም ብሓድሽ፡ እግዚኣብሄር ሓሳብ እቶም ጥበበኛታት ከንቱ ምዃኑ ይፈልጦ፡ ዚብል ጽሑፍ ኣሎ።
+            ጥበብ እዛ ዓለም እዚኣ ኣብ ኣምላኽ ዕሽነት እያ እሞ፡ ገለ ኻባኻትኩም ኣብዛ ዓለም እዚኣ ጥበበኛ ኸም ዝዀነ
+            እንተ መሰሎ፡ ጥበበኛ ምእንቲ ኪኸውንሲ ዓሻ ይኹን። 21-22 ጳውሎስ ወይ ኣጵሎስ፡ ኬፋ ዀነ ዓለም ኰነት፡
+            ህይወት ኰነት ሞት ኰነ፡ ዘሎ ዀነ ወይ ዚመጽእ፡ ኲሉ ናታትኩም እዩ እሞ፡ ሓደ እኳ ብሰብ ኣይመካሕ። 23
+            ንስኻትኩምሲ ናይ ክርስቶስ ደኣ ኢኹም፡ ክርስቶስውን ናይ ኣምላኽ እዩ።
+          </p>
+
+          <p className="mx-12"> 
+            1 Brothers, I couldn''t speak to you as to spiritual, but as to
+            fleshly, as to babies in Christ. 2 I fed you with milk, not with
+            meat; for you weren''t yet ready. Indeed, not even now are you
+            ready, 3 for you are still fleshly. For insofar as there is
+            jealousy, strife, and factions among you, aren''t you fleshly, and
+            don''t you walk in the ways of men? 4 For when one says, "I follow
+            Paul," and another, "I follow Apollos," aren''t you fleshly? 5 Who
+            then is Apollos, and who is Paul, but servants through whom you
+            believed; and each as the Lord gave to him? 6 I planted. Apollos
+            watered. But God gave the increase. 7 So then neither he who plants
+            is anything, nor he who waters, but God who gives the increase. 8
+            Now he who plants and he who waters are the same, but each will
+            receive his own reward according to his own labor. 9 For we are
+            God''s fellow workers. You are God''s farming, God''s building. 10
+            According to the grace of God which was given to me, as a wise
+            master builder I laid a foundation, and another builds on it. But
+            let each man be careful how he builds on it. 11 For no one can lay
+            any other foundation than that which has been laid, which is Jesus
+            Christ. 12 But if anyone builds on the foundation with gold, silver,
+            costly stones, wood, hay, or stubble; 13 each man''s work will be
+            revealed. For the Day will declare it, because it is revealed in
+            fire; and the fire itself will test what sort of work each man''s
+            work is. 14 If any man''s work remains which he built on it, he will
+            receive a reward. 15 If any man''s work is burned, he will suffer
+            loss, but he himself will be saved, but as through fire. 16 Don''t
+            you know that you are a temple of God, and that God''s Spirit lives
+            in you? 17 If anyone destroys the temple of God, God will destroy
+            him; for God''s temple is holy, which you are. 18 Let no one deceive
+            himself. If anyone thinks that he is wise among you in this world,
+            let him become a fool, that he may become wise. 19 For the wisdom of
+            this world is foolishness with God. For it is written, "He has taken
+            the wise in their craftiness." 20 And again, "The Lord knows the
+            reasoning of the wise, that it is worthless." 21 Therefore let no
+            one boast in men. For all things are yours, 22 whether Paul, or
+            Apollos, or Cephas, or the world, or life, or death, or things
+            present, or things to come. All are yours, 23 and you are Christ''s,
+            and Christ is God''s.
           </p>
         </div>
       </div>
